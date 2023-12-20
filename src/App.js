cbf0884fc3d38ca9ca7807173ea60e7e8753c15f
femmytedrey <femmytedrey@gmail.com>
2023-12-20 18:34:33 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { SideBar } from './SideBar'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <SideBar />
+    </>
+  )
+}
+
+export default App
